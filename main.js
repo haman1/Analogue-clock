4bf115ac631213e7b58a113b195fc28ff49ee70e
ombasa <ombasaherman@gmail.com>
2021-08-11 23:08:11 +0300

    clock face, numbers, Time & hand

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ drawClock();
 
 function drawClock() {
    drawFace(ctx, radius);
+   drawNumbers(ctx, radius);
 }
 
 function drawFace(ctx, radius) {
@@ -24,4 +25,24 @@ function drawFace(ctx, radius) {
     ctx.arc(0,0,radius*0.1,0,2*Math.PI);
     ctx.fillStyle = '#333';
     ctx.fill();
+}
+
+function drawNumbers() {
+    var ang;
+    var num;
+    ctx.font = radius*0.15 + "px arial";
+    ctx.textBaseline="middle";
+    ctx.textAlign="center";
+    for(num = 1; num <13; num++) {
+        ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius*0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(),0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius*0.85);
+        ctx.rotate(-ang);
+
+    }
+
 }
